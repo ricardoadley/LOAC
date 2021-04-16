5d25c03ef3c18770e4f4128e13166b16935a4c21
ricardoadley <ricardo.sena@ccc.ufcg.edu.br>
2021-04-16 20:18:53 -0300

    correção segmento

--- exercicioSegmento.sv
+++ exercicioSegmento.sv
@@ -1,7 +1,5 @@
-//119110702
 //Ricardo Adley da Silva Sena
-//DESCRIPTION: Exercicios display de 7 segmentos
-//Exercicos em : < http://lad.dsc.ufcg.edu.br/loac/index.php?n=OAC.Seg7 >
+//DESCRIPTION: Exercicios display de 7 segmentos Exercicos em : < http://lad.dsc.ufcg.edu.br/loac/index.php?n=OAC.Seg7 >
 
 parameter NINSTR_BITS = 32;
 parameter NBITS_TOP = 8, NREGS_TOP = 32, NBITS_LCD = 64;
@@ -33,11 +31,12 @@ module top(input  logic clk_2,
     lcd_a <= {56'h1234567890ABCD, SWI};
     lcd_b <= {SWI, 56'hFEDCBA09876543};
   end
+  parameter segmentos = 7;
   logic w;
   logic x;
   logic y;
   logic z;
-  logic [5:0] controlador ;
+  logic [segmentos-1:0] controlador ;
 always_comb begin
 	w <= SWI[0];
     	x <= SWI[1];
@@ -47,53 +46,53 @@ always_comb begin
 	
 	//Exibicao em hexadecimal
 	case(controlador[5:0])
-	0: SEG[6:0]<=7'b0111111;
-	1: SEG[6:0] <= 7'b0000110;
-        2: SEG[6:0] <= 7'b1011011;
-        3: SEG[6:0] <= 7'b1001111;
-        4: SEG[6:0] <= 7'b1100110;
-        5: SEG[6:0] <= 7'b1101101;
-        6: SEG[6:0] <= 7'b1111101;
-        7: SEG[6:0] <= 7'b0000111;
-        8: SEG[6:0] <= 7'b1111111;
-        9: SEG[6:0] <= 7'b1101111;
-	10: SEG[6:0] <= 7'b1110111;
-        11: SEG[6:0] <= 7'b1111100;
-        12: SEG[6:0] <= 7'b0111001;
-        13: SEG[6:0] <= 7'b1011110;
-        14: SEG[6:0] <= 7'b1111001;
-        15: SEG[6:0] <= 7'b1110001;
+	0: SEG[segmentos-1:0]<=7'b0111111;
+	1: SEG[segmentos-1:0] <= 7'b0000110;
+        2: SEG[segmentos-1:0] <= 7'b1011011;
+        3: SEG[segmentos-1:0] <= 7'b1001111;
+        4: SEG[segmentos-1:0] <= 7'b1100110;
+        5: SEG[segmentos-1:0] <= 7'b1101101;
+        6: SEG[segmentos-1:0] <= 7'b1111101;
+        7: SEG[segmentos-1:0] <= 7'b0000111;
+        8: SEG[segmentos-1:0] <= 7'b1111111;
+        9: SEG[segmentos-1:0] <= 7'b1101111;
+	10: SEG[segmentos-1:0] <= 7'b1110111;
+        11: SEG[segmentos-1:0] <= 7'b1111100;
+        12: SEG[segmentos-1:0] <= 7'b0111001;
+        13: SEG[segmentos-1:0] <= 7'b1011110;
+        14: SEG[segmentos-1:0] <= 7'b1111001;
+        15: SEG[segmentos-1:0] <= 7'b1110001;
         
         
         
         // Alfabeto
-        16: SEG[6:0] <= 7'b1110111; // A
-        17: SEG[6:0] <= 7'b1111100; // b
-        18: SEG[6:0] <= 7'b0111001; // C
-        19: SEG[6:0] <= 7'b1011000; // c
-        20: SEG[6:0] <= 7'b1011110; // d
-        21: SEG[6:0] <= 7'b1111001; // E
-        22: SEG[6:0] <= 7'b1110001; // F
-        23: SEG[6:0] <= 7'b1101111; // g
-        24: SEG[6:0] <= 7'b1110110; // H
-        25: SEG[6:0] <= 7'b1110100; // h
-        26: SEG[6:0] <= 7'b0000110; // I
-        27: SEG[6:0] <= 7'b0000100; // i
-        28: SEG[6:0] <= 7'b0011110; // J
-        29: SEG[6:0] <= 7'b0111000; // L
-        30: SEG[6:0] <= 7'b1010100; // n
-        31: SEG[6:0] <= 7'b0111111; // O
-        32: SEG[6:0] <= 7'b1011100; // o
-        33: SEG[6:0] <= 7'b1110011; // P
-        34: SEG[6:0] <= 7'b1100111; // q
-        35: SEG[6:0] <= 7'b1010000; // r
-        36: SEG[6:0] <= 7'b1101101; // S
-        37: SEG[6:0] <= 7'b1111000; // t
-        38: SEG[6:0] <= 7'b0111110; // U
-        39: SEG[6:0] <= 7'b0011100; // u
-        40: SEG[6:0] <= 7'b1101110; // y
-        41: SEG[6:0] <= 7'b1100011; // º 
-	default: SEG[6:0] <= 7'b1000000;  
+        16: SEG[segmentos-1:0] <= 7'b1110111; // A
+        17: SEG[segmentos-1:0] <= 7'b1111100; // b
+        18: SEG[segmentos-1:0] <= 7'b0111001; // C
+        19: SEG[segmentos-1:0] <= 7'b1011000; // c
+        20: SEG[segmentos-1:0] <= 7'b1011110; // d
+        21: SEG[segmentos-1:0] <= 7'b1111001; // E
+        22: SEG[segmentos-1:0] <= 7'b1110001; // F
+        23: SEG[segmentos-1:0] <= 7'b1101111; // g
+        24: SEG[segmentos-1:0] <= 7'b1110110; // H
+        25: SEG[segmentos-1:0] <= 7'b1110100; // h
+        26: SEG[segmentos-1:0] <= 7'b0000110; // I
+        27: SEG[segmentos-1:0] <= 7'b0000100; // i
+        28: SEG[segmentos-1:0] <= 7'b0011110; // J
+        29: SEG[segmentos-1:0] <= 7'b0111000; // L
+        30: SEG[segmentos-1:0] <= 7'b1010100; // n
+        31: SEG[segmentos-1:0] <= 7'b0111111; // O
+        32: SEG[segmentos-1:0] <= 7'b1011100; // o
+        33: SEG[segmentos-1:0] <= 7'b1110011; // P
+        34: SEG[segmentos-1:0] <= 7'b1100111; // q
+        35: SEG[segmentos-1:0] <= 7'b1010000; // r
+        36: SEG[segmentos-1:0] <= 7'b1101101; // S
+        37: SEG[segmentos-1:0] <= 7'b1111000; // t
+        38: SEG[segmentos-1:0] <= 7'b0111110; // U
+        39: SEG[segmentos-1:0] <= 7'b0011100; // u
+        40: SEG[segmentos-1:0] <= 7'b1101110; // y
+        41: SEG[segmentos-1:0] <= 7'b1100011; // º 
+	default: SEG[segmentos-1:0] <= 7'b1000000;  
     endcase
   end
   
@@ -108,32 +107,32 @@ always_comb begin
 	if(opcao)
 	case(notas[3:0])
 	//mostra situacao P,A,F
-		0: SEG[6:0]<=7'b1110011;
-		1: SEG[6:0] <= 7'b1110011;
-		2: SEG[6:0] <= 7'b1110011;
-		3: SEG[6:0] <= 7'b1110011;
-		4: SEG[6:0] <= 7'b1110001;
-		5: SEG[6:0] <= 7'b1110001;
-		6: SEG[6:0] <= 7'b1110001;
-		7: SEG[6:0] <= 7'b1110111;
-		8: SEG[6:0] <= 7'b1110111;
-		9: SEG[6:0] <= 7'b1110111;
-		default: SEG[6:0] <= 7'b1000000; 
+		0: SEG[segmentos-1:0]<=7'b1110011;
+		1: SEG[segmentos-1:0] <= 7'b1110011;
+		2: SEG[segmentos-1:0] <= 7'b1110011;
+		3: SEG[segmentos-1:0] <= 7'b1110011;
+		4: SEG[segmentos-1:0] <= 7'b1110001;
+		5: SEG[segmentos-1:0] <= 7'b1110001;
+		6: SEG[segmentos-1:0] <= 7'b1110001;
+		7: SEG[segmentos-1:0] <= 7'b1110111;
+		8: SEG[segmentos-1:0] <= 7'b1110111;
+		9: SEG[segmentos-1:0] <= 7'b1110111;
+		default: SEG[segmentos-1:0] <= 7'b1000000; 
         endcase
         else 
         	case(notas[3:0])
         	//mostra valor notas
-		0: SEG[6:0]<=7'b0111111;
-		1: SEG[6:0] <= 7'b0000110;
-		2: SEG[6:0] <= 7'b1011011;
-		3: SEG[6:0] <= 7'b1001111;
-		4: SEG[6:0] <= 7'b1100110;
-		5: SEG[6:0] <= 7'b1101101;
-		6: SEG[6:0] <= 7'b1111101;
-		7: SEG[6:0] <= 7'b0000111;
-		8: SEG[6:0] <= 7'b1111111;
-		9: SEG[6:0] <= 7'b1101111;
-		default: SEG[6:0] <= 7'b1000000; 
+		0: SEG[segmentos-1:0]<=7'b0111111;
+		1: SEG[segmentos-1:0] <= 7'b0000110;
+		2: SEG[segmentos-1:0] <= 7'b1011011;
+		3: SEG[segmentos-1:0] <= 7'b1001111;
+		4: SEG[segmentos-1:0] <= 7'b1100110;
+		5: SEG[segmentos-1:0] <= 7'b1101101;
+		6: SEG[segmentos-1:0] <= 7'b1111101;
+		7: SEG[segmentos-1:0] <= 7'b0000111;
+		8: SEG[segmentos-1:0] <= 7'b1111111;
+		9: SEG[segmentos-1:0] <= 7'b1101111;
+		default: SEG[segmentos-1:0] <= 7'b1000000; 
         endcase
         end
 
